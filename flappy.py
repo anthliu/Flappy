2bc387d742341d2d7afcd3c9c5f88ce09ed3a18f
liu916 <zliu916@gmail.com>
2014-03-09 17:32:37 -0400

    updating flappy.py

--- flappy.py
+++ flappy.py
@@ -1,25 +1,21 @@
 import pygame, sys
 import pygame.locals as py
+from sprites import *
 
 FPS = 30 #Frames per second
-SPD = 5 #Speed of the bird traveling right
-ACC = -3 #Acceleration downward
-SCL = 4 #how scaled the sprites are
+bird_speed = 5
+acceleration = -3 #Acceleration downward
+ground_height = 80
 
 def main():
     pygame.init()
     DISPLAY = pygame.display.set_mode((640, 480), 0, 32)
 
-    sheet = pygame.image.load('sprites.png')
-    
-    sheet.set_clip(pygame.Rect(264,64,17,12))
-    rawBird = sheet.subsurface(sheet.get_clip())
-    rawBirdSize = rawBird.get_rect().size
-    bird = pygame.transform.scale(rawBird, (rawBirdSize[0] * SCL, rawBirdSize[1] * SCL))
-
     while True:
         DISPLAY.fill((255,255,255))
         DISPLAY.blit(bird, (0, 0))
+        DISPLAY.blit(upper_pipe, (200, 0))
+        DISPLAY.blit(lower_pipe, (400, 0))
         for event in pygame.event.get():
             if event.type == py.QUIT:
                 pygame.quit()
